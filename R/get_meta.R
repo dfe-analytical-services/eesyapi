c903c563f2054cc6917965abf56df13d9f7abf3f
Rich Bielby <Richard.BIELBY@education.gov.uk>
2024-09-04 16:57:31 +0100

    Expanded get_meta function to extract to json parsed structured list

--- R/get_meta.R
+++ R/get_meta.R
@@ -3,22 +3,35 @@
 #' @param dataset_id ID of dataset to be connected to
 #' @param dataset_version Version of dataset to be connected to
 #' @param api_version EES API version
+#' @param parsed
 #'
 #' @return Results of query to meta data
 #' @export
 #'
 #' @examples
 #' get_meta("d7329101-f275-d277-bbfe-d8cfaa709833")
-get_meta <- function(dataset_id, dataset_version=NULL, api_version=NULL){
+get_meta <- function(dataset_id, dataset_version = NULL, api_version = NULL, parsed = TRUE) {
   meta_url <- eesapi_url(
     endpoint = "get-meta",
     dataset_id = dataset_id,
-    dataset_version=dataset_version
+    dataset_version = dataset_version
   )
   response <- httr::GET(meta_url)
-  if(response$status_code == 200){
-    return(response %>% httr::content())
+  if (response$status_code == 200) {
+    if (parsed) {
+      result <- response %>%
+        httr::content("text") %>%
+        jsonlite::fromJSON()
+    } else {
+      result <- response
+    }
+    return(result)
   } else {
-    stop(paste0("Query returned error, status: ", response$status))
+    stop(paste0(
+      "Query returned error, status: ",
+      response$status,
+      "\n      ",
+      http_request_error(response$status)
+    ))
   }
 }
---
+++ R/helper_functions.R
@@ -0,0 +1,20 @@
+#' http request error
+#'
+#' @description
+#' Translate a http error code into an error message.
+#'
+#' @param response_status Response code from an API request
+#'
+#' @return Translation of the response code
+#' @export
+#'
+#' @examples
+#' http_request_error(200)
+http_request_error <- function(response_status) {
+  status_lookup <- list(
+    "200" = "Successful API request.",
+    "400" = "Invalid query submitted to API.",
+    "500" = "Internal server error encountered."
+  )
+  status_lookup[format(round(response_status, -1))][[1]]
+}
