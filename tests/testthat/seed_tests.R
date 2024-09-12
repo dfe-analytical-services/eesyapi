1ffaf6db42cbac813fb34bcafcdde87661635f4a
Rich Bielby <Richard.BIELBY@education.gov.uk>
2024-09-12 14:59:12 +0100

    Added explicit eesyapi references to seed_tests.R

--- tests/testthat/seed_tests.R
+++ tests/testthat/seed_tests.R
@@ -1,18 +1,18 @@
-seed_all_tests <- function() {
+seed_tests <- function() {
   seed_get_publication()
   seed_get_publication_datasets()
 }
 
 seed_get_publication <- function() {
   saveRDS(
-    get_publication_catalogue(),
+    eesyapi::get_publication_catalogue(),
     file = "tests/testthat/testdata/example_publication_catalogue.rds"
   )
 }
 
 seed_get_publication_datasets <- function() {
   saveRDS(
-    get_publication_datasets(example_id("publication")),
+    eesyapi::get_publication_datasets(example_id("publication")),
     file = "tests/testthat/testdata/example_publication_datasets.rds"
   )
 }
