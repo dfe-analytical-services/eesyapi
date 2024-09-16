cb6aaa8002a26aea27044b55247873fdb63c9711
Rich Bielby <Richard.BIELBY@education.gov.uk>
2024-09-16 13:40:00 +0100

    Splitting api_url_pages into it's own script

--- R/api_url.R
+++ R/api_url.R
@@ -156,33 +156,3 @@ api_url <- function(
   return(url)
 }
 
-#' EES API URL paging
-#'
-#' @param page_size Number of results to return in a single query (max 40)
-#' @param page Page number to return
-#'
-#' @return String containing pages query
-#' @export
-#'
-#' @examples
-#' api_url_pages()
-#' api_url_pages(page_size = 20, page = 2)
-api_url_pages <- function(page_size = 40, page = NULL) {
-  paste0(
-    ifelse(
-      !is.null(page),
-      paste0("page=", page),
-      ""
-    ),
-    ifelse(
-      !is.null(page) & !is.null(page_size),
-      paste0("&"),
-      ""
-    ),
-    ifelse(
-      !is.null(page_size),
-      paste0("pageSize=", page_size),
-      ""
-    )
-  )
-}
